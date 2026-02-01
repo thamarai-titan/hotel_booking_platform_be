80de37fe19256bdd20eb8939e6ce75b118aba283
Thamarai <tmanalan710@gmail.com>
2026-02-01 23:38:07 +0530

    completed: loginController

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,16 @@
 import type {Request, Response} from "express"
-import { signupSchema, type signupSchemaType } from "../schemas/auth.schema"
-import { createUser } from "../services/auth.service"
+import { loginSchema, signupSchema, type loginSchemaType, type signupSchemaType } from "../schemas/auth.schema"
+import { createUser, loginUser } from "../services/auth.service"
 import { Prisma } from "@prisma/client"
+import { success } from "zod"
+import { use } from "react"
+import { signJWT } from "../utils/jwt"
 
 
 export const SignUpController = async (req:Request, res:Response)=>{
-    const data:signupSchemaType = signupSchema.parse(req.body)
+    
     try {
+        const data:signupSchemaType = signupSchema.parse(req.body)
         const user = await createUser(data)
         res.status(201).json({
             success: true,
@@ -45,3 +49,46 @@ export const SignUpController = async (req:Request, res:Response)=>{
         })
     }
 }   
+
+export const loginController = async (req:Request, res:Response)=>{
+    
+    try {
+        const data:loginSchemaType = loginSchema.parse(req.body)
+        const user = await loginUser(data)
+        
+        const token = await signJWT({userId: user.id})
+        res.status(200).json({
+            success: true,
+            data: {
+                token: token,
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    role: user.role
+                }
+            },
+            error:null
+        })
+    } catch (error: any) {
+        if(error?.name === "INVALID_CREDENTIALS"){
+            return res.status(401).json({
+                success:false,
+                data: null,
+                error: "INVALID_CREDENTIALS"
+            })
+        }
+        if(error?.name === "ZodError"){
+            return res.status(400).json({
+                success:false,
+                data: null,
+                error: "INVALID_REQUEST"
+            })
+        }
+        res.status(500).json({
+            success: true,
+            data: null,
+            error: "INTERNAL_SERVER_ERROR"
+        })
+    }
+}
