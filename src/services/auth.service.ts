0feb26bce48549067c0c5bf3c8eb31a1454c1565
Thamarai <tmanalan710@gmail.com>
2026-02-01 23:38:50 +0530

    completed: prisma service for login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { da } from "zod/locales"
 import { prisma } from "../db/prisma.ts"
-import type { signupSchemaType } from "../schemas/auth.schema.ts"
+import type { loginSchemaType, signupSchemaType } from "../schemas/auth.schema.ts"
 import bcrypt from 'bcrypt'
 
 export const createUser = async (data:signupSchemaType)=>{
@@ -25,8 +25,30 @@ export const createUser = async (data:signupSchemaType)=>{
     })
     return user
     } catch (error) {
-        throw new Error("User could not be created")
+        throw error
     }
 
     
 }
+
+export const loginUser = async (data:loginSchemaType)=>{
+    const {email,password} = data
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    })
+
+    if(!user){
+        throw new Error("INVALID_CREDENTIALS")
+    }
+
+    const inValidPassword = await bcrypt.compare(data.password, user.password)
+
+    if(!inValidPassword){
+        throw new Error("INVALID_CREDENTIALS")
+    }
+
+    return user
+}   
