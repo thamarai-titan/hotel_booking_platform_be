26961a3951d857e9ff9c1d5e66323fa79493f61c
Thamarai <tmanalan710@gmail.com>
2026-02-02 19:08:27 +0530

    added: role checker

---
+++ src/middleware/middleware.ts
@@ -0,0 +1,48 @@
+import type { NextFunction, Request, Response } from "express"
+import jwt from "jsonwebtoken"
+
+declare global {
+    namespace Express {
+        interface Request {
+                userId: string;
+                role: string;
+        }
+    }
+}
+export const verifyUser = async (req:Request, res:Response, next: NextFunction)=>{
+    const authHeader = req.headers.authorization
+
+    if(!authHeader || !authHeader.startsWith("Bearer")){
+        return res.status(401).json({
+            success:false,
+            data:null,
+            error: "UNAUTHORIZED"
+        })
+    }
+    const token = authHeader.split(" ")[1] as string;
+    try {
+        const decoded  = jwt.verify(token, process.env.JWT_SECRET!) as {
+            role: string;userId: string
+}
+        req.userId = decoded.userId
+        req.role = decoded.role
+        next()
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            data: null,
+            error: "UNAUTHORIZED"
+        })
+    }
+}   
+
+export const requireRole = (req:Request, res:Response, next:NextFunction)=>{
+    if(req.role !== "owner"){
+        return res.status(403).json({
+            success:false,
+            data: null,
+            error: "FORBIDDEN"
+        })
+    }
+    next()
+}
