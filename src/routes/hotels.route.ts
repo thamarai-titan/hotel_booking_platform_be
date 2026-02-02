fec6cb8c18d81e0227972f1fa83031ea540c1155
Thamarai <tmanalan710@gmail.com>
2026-02-02 21:37:53 +0530

    completed: room created route

--- src/routes/hotels.route.ts
+++ src/routes/hotels.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { requireRole, verifyUser } from "../middleware/middleware";
 import { createHotelController } from "../controllers/hotels.controller";
+import { addRoomtoHotels } from "../controllers/rooms.controller";
 
 const router = Router()
 
 router.post("/hotels", verifyUser, requireRole, createHotelController)
+router.post("/hotels/:hotelId/rooms", verifyUser, requireRole, addRoomtoHotels)
 
 export default router
