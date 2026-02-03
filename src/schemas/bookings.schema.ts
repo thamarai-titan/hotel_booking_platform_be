8156bbb92f8ccf8fb3c816f77350f5d3dfd072e6
Thamarai <tmanalan710@gmail.com>
2026-02-03 23:09:06 +0530

    added: schemas for the bookings

---
+++ src/schemas/bookings.schema.ts
@@ -0,0 +1,10 @@
+import z from 'zod'
+
+export const bookingSchema = z.object({
+    roomId: z.string().min(1,"provide the roomId"),
+    checkInDate: z.date(),
+    checkOutDate: z.date(),
+    guests: z.number().min(1,"Atleast a guest needed")
+})
+
+export type bookingSchemaType = z.infer<typeof bookingSchema>
