726fee2607faef1b4c812b0cb30f092605019733
Thamarai <tmanalan710@gmail.com>
2026-02-03 18:56:56 +0530

    added: getAll route

--- src/routes/hotels.route.ts
+++ src/routes/hotels.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { requireRole, verifyUser } from "../middleware/middleware";
-import { createHotelController } from "../controllers/hotels.controller";
+import { createHotelController, getAllHotelsController } from "../controllers/hotels.controller";
 import { addRoomtoHotels } from "../controllers/rooms.controller";
 
 const router = Router()
 
 router.post("/hotels", verifyUser, requireRole, createHotelController)
 router.post("/hotels/:hotelId/rooms", verifyUser, requireRole, addRoomtoHotels)
+router.get("/hotels",verifyUser, requireRole, getAllHotelsController)
 
 export default router
