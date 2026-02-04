6ea284479816a5c6a2cc8f36888bb9f49adbcb0d
Thamarai <tmanalan710@gmail.com>
2026-02-04 21:39:57 +0530

    added: bookingAuth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
-import authRoute from './routes/auth.route.ts'
+import authRoutes from './routes/auth.route.ts'
+import hotelRoutes from './routes/hotels.route.ts'
+import bookingAuth from './routes/bookings.route.ts'
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -8,7 +10,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoutes)
+app.use("/api", hotelRoutes)
+app.use("/api", bookingAuth)
 
 app.listen(PORT, ()=>{
     console.log(`Server Running on ${PORT}`)
