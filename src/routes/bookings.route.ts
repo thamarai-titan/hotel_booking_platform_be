4699a9887266ce1ad72a08ee2e7e17ca7ac8deb0
Thamarai <tmanalan710@gmail.com>
2026-02-04 21:19:06 +0530

    added: routes for getallbookings

--- src/routes/bookings.route.ts
+++ src/routes/bookings.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { requireCustomer, verifyUser } from "../middleware/middleware";
+import { createBookingsController, getAllBookingsController } from "../controllers/bookings.controller";
 
 const router = Router()
 
 
-
+router.post("/bookings", verifyUser, requireCustomer, createBookingsController)
+router.get("/bookings", verifyUser, requireCustomer, getAllBookingsController)
 
 export default router
