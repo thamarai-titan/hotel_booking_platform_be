0895287beb808dbf40a53154118e1da2cb6465ea
Thamarai <tmanalan710@gmail.com>
2026-02-04 23:45:19 +0530

    added: route for booking cancel

--- src/routes/bookings.route.ts
+++ src/routes/bookings.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { requireCustomer, verifyUser } from "../middleware/middleware";
-import { createBookingsController, getAllBookingsController } from "../controllers/bookings.controller";
+import { createBookingsController, getAllBookingsController, modifiedBookingController } from "../controllers/bookings.controller";
 
 const router = Router()
 
 
 router.post("/bookings", verifyUser, requireCustomer, createBookingsController)
 router.get("/bookings", verifyUser, requireCustomer, getAllBookingsController)
-
+router.post("/bookings/:bookingId/cancel", verifyUser, requireCustomer, modifiedBookingController)
 export default router
