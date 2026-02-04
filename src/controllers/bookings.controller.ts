3024a1f212d5af33fadeb2b2c846a2ec6a7901bd
Thamarai <tmanalan710@gmail.com>
2026-02-04 21:16:05 +0530

    added: getAllBookingsController

--- src/controllers/bookings.controller.ts
+++ src/controllers/bookings.controller.ts
@@ -5,9 +5,16 @@ import {
 } from "../schemas/bookings.schema";
 import {
   createBookings,
+  getAllBookingsFromCurrentUser,
   getHotelDetailsWithroomId,
 } from "../services/bookings.service";
+import type { Prisma } from "@prisma/client";
+import { da } from "zod/locales";
 
+
+type Params = {
+  status: string;
+}
 
 export const createBookingsController = async (req: Request, res: Response) => {
   try {
@@ -36,7 +43,7 @@ export const createBookingsController = async (req: Request, res: Response) => {
       data,
       totalPrice,
       req.userId,
-      hotelRoomData.id,
+      hotelRoomData.hotel_id,
     );
 
     if (!booking) {
@@ -61,6 +68,7 @@ export const createBookingsController = async (req: Request, res: Response) => {
         status: booking.status,
         bookingDate: booking.booking_date,
       },
+      error: null
     });
   } catch (error: any) {
     if (error?.name === "ZodError") {
@@ -72,9 +80,47 @@ export const createBookingsController = async (req: Request, res: Response) => {
     }
 
     res.status(400).json({
-        success: false,
-        data: null,
-        error: "INVALID_REQUEST"
+      success: false,
+      data: null,
+      error: "INVALID_REQUEST",
+    });
+  }
+};
+
+export const getAllBookingsController = async (req: Request<Params>, res: Response) => {
+  try {
+    const userId = req.userId;
+    let { status } = req.query
+    
+    const UserBookings = await getAllBookingsFromCurrentUser(userId,String(status))
+
+    const data = UserBookings.map(d => ({
+      id: d.id,
+      roomId: d.room_id,
+      hotelId: d.hotel_id,
+      hotelName: d.hotels.name,
+      roomNumber: d.rooms.room_number,
+      roomType: d.rooms.room_type,
+      checkInDate: d.check_in_date,
+      checkOutDate: d.check_out_date,
+      guests: d.guests,
+      totalPrice: Number(d.total_price),
+      status: d.status,
+      bookingDate: d.booking_date
+    }))
+
+    res.status(200).json({
+      success: true,
+      data: data,
+      error: null
     })
+  } catch (error) {
+
+    res.status(400).json({
+      success: true,
+      data: null,
+      error: "ERROR"
+    })
+
   }
 };
