84c5d57ef8d17fc9fdd02cb03968b295048cb9c6
Thamarai <tmanalan710@gmail.com>
2026-02-04 21:24:02 +0530

    added: routes for hotels

--- src/routes/hotels.route.ts
+++ src/routes/hotels.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { requireRole, verifyUser } from "../middleware/middleware";
+import { requireOwner, verifyUser } from "../middleware/middleware";
 import { createHotelController, getAllHotelsController, getAllHotelsWithHotelIdController } from "../controllers/hotels.controller";
 import { addRoomtoHotels } from "../controllers/rooms.controller";
 
 const router = Router()
 
-router.post("/hotels", verifyUser, requireRole, createHotelController)
-router.post("/hotels/:hotelId/rooms", verifyUser, requireRole, addRoomtoHotels)
-router.get("/hotels",verifyUser, requireRole, getAllHotelsController)
+router.post("/hotels", verifyUser, requireOwner, createHotelController)
+router.post("/hotels/:hotelId/rooms", verifyUser, requireOwner, addRoomtoHotels)
+router.get("/hotels",verifyUser, requireOwner, getAllHotelsController)
 router.get("/hotels/:hotelId", verifyUser, getAllHotelsWithHotelIdController)
 
 
