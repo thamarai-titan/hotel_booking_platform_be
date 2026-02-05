4a522af1b9a259869aa24993df75311899266e09
Thamarai <tmanalan710@gmail.com>
2026-02-05 21:02:53 +0530

    added reviewAuth

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import authRoutes from './routes/auth.route.ts'
 import hotelRoutes from './routes/hotels.route.ts'
 import bookingAuth from './routes/bookings.route.ts'
+import reviewAuth from './routes/reviews.auth.ts'
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api", hotelRoutes)
 app.use("/api", bookingAuth)
+app.use("/api", reviewAuth)
 
 app.listen(PORT, ()=>{
     console.log(`Server Running on ${PORT}`)
